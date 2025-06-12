aba90f86f589365c2adc3332d54d7d38046ce01d
Erkai Yu <erkaiyu2@illinois.edu>
2025-06-12 13:31:19 -0400

    indentation

--- contrib/plugins/runtime_patch_monitor.c
+++ contrib/plugins/runtime_patch_monitor.c
@@ -29,36 +29,36 @@ static size_t buffer_size;
 QEMU_PLUGIN_EXPORT int qemu_plugin_version = QEMU_PLUGIN_VERSION;
 
 static void vcpu_mem(unsigned int cpu_index, qemu_plugin_meminfo_t info,
-                     uint64_t vaddr, void *udata)
+        uint64_t vaddr, void *udata)
 {
-	if (!qemu_plugin_mem_is_store(info)) return;
-	struct qemu_plugin_hwaddr *hwaddr;
-	hwaddr = qemu_plugin_get_hwaddr(info, vaddr);
-	if (hwaddr) {
-		uint64_t addr = qemu_plugin_hwaddr_phys_addr(hwaddr);
-		if (addr >= haddr_lo && addr < haddr_hi) {
-			rewrite_cnt++;	
-			
-			// Dump the physical addr
-			memcpy(buffer_ptr, &addr, 8);
-			
-			buffer_ptr += 8;
-			buffer_remaining_size -= 8;
+    if (!qemu_plugin_mem_is_store(info)) return;
+    struct qemu_plugin_hwaddr *hwaddr;
+    hwaddr = qemu_plugin_get_hwaddr(info, vaddr);
+    if (hwaddr) {
+        uint64_t addr = qemu_plugin_hwaddr_phys_addr(hwaddr);
+        if (addr >= haddr_lo && addr < haddr_hi) {
+            rewrite_cnt++;	
 
-			// Dump the instr stored in this addr
-			GByteArray *data = g_byte_array_sized_new(4);
+            // Dump the physical addr
+            memcpy(buffer_ptr, &addr, 8);
 
-			uint32_t instr = 0xaaaaaaaa; // Magic number
-			bool success = qemu_plugin_read_memory_vaddr(vaddr, data, 4);
-	
-			if (success) instr = data->data[0] + (data->data[1]<<8) + (data->data[2]<<16) + (data->data[3]<<24);
-			
-			memcpy(buffer_ptr, &instr, 4);
+            buffer_ptr += 8;
+            buffer_remaining_size -= 8;
 
-			buffer_ptr += 4;
-			buffer_remaining_size -= 4;
-		}
-	}
+            // Dump the instr stored in this addr
+            GByteArray *data = g_byte_array_sized_new(4);
+
+            uint32_t instr = 0xaaaaaaaa; // Magic number
+            bool success = qemu_plugin_read_memory_vaddr(vaddr, data, 4);
+
+            if (success) instr = data->data[0] + (data->data[1]<<8) + (data->data[2]<<16) + (data->data[3]<<24);
+
+            memcpy(buffer_ptr, &instr, 4);
+
+            buffer_ptr += 4;
+            buffer_remaining_size -= 4;
+        }
+    }
 }
 
 /**
@@ -89,52 +89,52 @@ static void vcpu_tb_trans(qemu_plugin_id_t id, struct qemu_plugin_tb *tb)
 
         /* Register callback on memory read or write */
         qemu_plugin_register_vcpu_mem_cb(insn, vcpu_mem,
-                                             QEMU_PLUGIN_CB_NO_REGS,
-                                             QEMU_PLUGIN_MEM_W, NULL);
+                QEMU_PLUGIN_CB_NO_REGS,
+                QEMU_PLUGIN_MEM_W, NULL);
         //g_free(insn_disas);
     }
 }
 
 static void plugin_exit(qemu_plugin_id_t id, void *p)
 {
-        fprintf(stderr, "\n");
-        FILE *f = fopen(dump_file, "w");
-        if (f) {
-                fwrite(buffer, 1, buffer_size - buffer_remaining_size, f);
-                fclose(f);
-        } else {
-                fprintf(stderr, "runtime_patch_monitor.c:%d: Could not open %s for writing\n", 
-                                __LINE__, dump_file);
-        }
-        free(buffer);
+    fprintf(stderr, "\n");
+    FILE *f = fopen(dump_file, "w");
+    if (f) {
+        fwrite(buffer, 1, buffer_size - buffer_remaining_size, f);
+        fclose(f);
+    } else {
+        fprintf(stderr, "runtime_patch_monitor.c:%d: Could not open %s for writing\n", 
+                __LINE__, dump_file);
+    }
+    free(buffer);
 
-        fprintf(stderr, "libruntime_patch_monitor.so:%s: rewrite_cnt=%lld\n", __FUNCTION__, 
-                                rewrite_cnt);
-        fprintf(stderr, "\n");
+    fprintf(stderr, "libruntime_patch_monitor.so:%s: rewrite_cnt=%lld\n", __FUNCTION__, 
+            rewrite_cnt);
+    fprintf(stderr, "\n");
 }
 
 /**
  * Install the plugin
  */
 QEMU_PLUGIN_EXPORT int qemu_plugin_install(qemu_plugin_id_t id,
-                                           const qemu_info_t *info, int argc,
-                                           char **argv)
+        const qemu_info_t *info, int argc,
+        char **argv)
 {
-	// Set up buffer
-	buffer_size = (size_t) 10 * 1024 * 1024 * 1024;
-	buffer = malloc(buffer_size);
+    // Set up buffer
+    buffer_size = (size_t) 10 * 1024 * 1024 * 1024;
+    buffer = malloc(buffer_size);
 
     if (buffer == NULL) {
-		fprintf(stderr, "runtime_patch_monitor.c:%d: Memory allocation failed\n", __LINE__);
-                return 1;
+        fprintf(stderr, "runtime_patch_monitor.c:%d: Memory allocation failed\n", __LINE__);
+        return 1;
     }
 
     buffer_remaining_size = buffer_size;
     buffer_ptr = buffer;
-	
-	// Register callbacks
+
+    // Register callbacks
     qemu_plugin_register_vcpu_tb_trans_cb(id, vcpu_tb_trans);
-	qemu_plugin_register_atexit_cb(id, plugin_exit, NULL);
+    qemu_plugin_register_atexit_cb(id, plugin_exit, NULL);
 
     return 0;
 }
